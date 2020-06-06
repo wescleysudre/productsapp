ba58586425d90dc33cae988e033e2bfabbc7f298
wescleysudre <wescley.sudre@sage.com>
2020-06-06 01:58:30 -0300

    criando serviços para consumir endpoints de produto

--- src/app/interfaces/product.ts
+++ src/app/interfaces/product.ts
@@ -2,7 +2,7 @@ import { Department } from './department';
 
 export interface Product {
   name: string;
-  departments: Department[];
+  departments: Department[] | string[];
   stock: number;
   price: number;
   _id ?: string;
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from 'src/app/interfaces/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  readonly url = 'http://localhost:3000/products';
+  private productsSubject$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>(null);
+  private loaded: boolean = false;
+
+  constructor(private http: HttpClient) { }
+
+  get(): Observable<Product[]> {
+    if (!this.loaded) {
+        this.http.get<Product[]>(this.url)
+          .subscribe(this.productsSubject$);
+      this.loaded = true;
+    }
+    return this.productsSubject$.asObservable();
+  }
 }
